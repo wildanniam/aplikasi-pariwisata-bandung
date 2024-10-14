092eb9bd55d1cab321e6238ad27bae4521decc0b
wildanniam <wildanniam4@gmail.com>
2024-10-14 16:00:42 +0700

    init projek

---
+++ android/app/src/main/kotlin/com/example/aplikasi_pariwisata_bandung/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplikasi_pariwisata_bandung
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:aplikasi_pariwisata_bandung/pages/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
